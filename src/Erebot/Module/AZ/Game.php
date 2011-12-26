76d34a8eeae13e18da45d1ee6040521a39237eaa
François Poirotte <clicky@erebot.net>
2011-12-26 21:46:50 +0100

    CS fix.

--- src/Erebot/Module/AZ/Game.php
+++ src/Erebot/Module/AZ/Game.php
@@ -246,7 +246,10 @@ class Erebot_Module_AZ_Game
     public function proposeWord($word)
     {
         Erebot_Module_Wordlists_Wordlist::normalizeWord(
-            $word, NULL, array('UTF-8', NULL));
+            $word,
+            NULL,
+            array('UTF-8', NULL)
+        );
         $ok = $this->_isValidWord($word);
 
         if ($ok === NULL)
